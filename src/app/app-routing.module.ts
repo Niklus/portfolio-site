c1e00d065ca34d898b9081211f89a6bdd0df608e
Nicholas Otieno <niklasoti@gmail.com>
2020-04-13 19:09:38 +0200

    added global styles and completed app navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: HomePageComponent 
+  },
+  { 
+    path: 'projects', 
+    component: ProjectsComponent 
+  },
+  { 
+    path: 'education', 
+    component: EducationComponent 
+  },
+  { 
+    path: 'contact', 
+    component: ContactComponent 
+  },
+  { 
+    path: '**', 
+    component: HomePageComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomePageComponent,
+    ProjectsComponent,
+    ContactComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
