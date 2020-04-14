3256755cf624d1df7bcb7c02dc8b9aeb030b6ba3
Nicholas Otieno <niklasoti@gmail.com>
2020-04-14 20:05:26 +0200

    Added firebase analytics and hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyArPlvB8MNyklCDt3eDIU2UNKNHMLQd0rg",
+    authDomain: "ng-portfolio-site.firebaseapp.com",
+    databaseURL: "https://ng-portfolio-site.firebaseio.com",
+    projectId: "ng-portfolio-site",
+    storageBucket: "ng-portfolio-site.appspot.com",
+    messagingSenderId: "516354558136",
+    appId: "1:516354558136:web:4a8b3537ba71b1aab60c4d",
+    measurementId: "G-SLVS5VX2J4"
+  }
 };
